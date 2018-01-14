39634528286afd3240a5e667e711c9056bea5dd5
DarkFlare <flaregamingxx@gmail.com>
2018-01-14 11:04:01 -0600

    Initial commit

---
+++ Byte Converter/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Byte_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button button2;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            button2.Location = new System.Drawing.Point(12, 414);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(306, 49);
+            button2.TabIndex = 3;
+            button2.Text = "Load Little Endian File";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 32);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(306, 376);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Consolas", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(595, 32);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(306, 376);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(324, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(265, 49);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Convert to Big Endian -->";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(595, 414);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(306, 49);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Load Big Endian File";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Little Endian:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(591, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Big Endian:";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(324, 262);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(265, 49);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "<-- Convert to Little Endian";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 25);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(73, 24);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "16 bit";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 55);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(73, 24);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "32 bit";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 85);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(73, 24);
+            this.radioButton3.TabIndex = 10;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "64 bit";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(324, 51);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(265, 122);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Data size:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(913, 475);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Byte Converter";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Byte Converter/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Byte_Converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public String ChangeBytes16(String input)
+        {
+            input = input.Replace(" ", "");
+            String output = "";
+            for (int i = 0; i < input.Length; i += 4)
+            {
+                short tmp = Convert.ToInt16(input.Substring(i, 4), 16);
+                short reversedBytes = System.Net.IPAddress.NetworkToHostOrder(tmp);
+                String start = reversedBytes.ToString("X").PadLeft(4, '0');
+                output += start;
+            }
+            return (output);
+        }
+
+        public String ChangeBytes32(String input)
+        {
+            input = input.Replace(" ", "");
+            String output = "";
+            for (int i = 0; i < input.Length; i += 8)
+            {
+                int tmp = Convert.ToInt32(input.Substring(i, 8), 16);
+                int reversedBytes = System.Net.IPAddress.NetworkToHostOrder(tmp);
+                String start = reversedBytes.ToString("X").PadLeft(8, '0');
+                output += start;
+            }
+            return (output);
+        }
+
+        public String ChangeBytes64(String input)
+        {
+            input = input.Replace(" ", "");
+            String output = "";
+            for (int i = 0; i < input.Length; i += 16)
+            {
+                long tmp = Convert.ToInt64(input.Substring(i, 16), 16);
+                long reversedBytes = System.Net.IPAddress.NetworkToHostOrder(tmp);
+                String start = reversedBytes.ToString("X").PadLeft(16, '0');
+                output += start;
+            }
+            return (output);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+                textBox2.Text = ChangeBytes16(textBox1.Text);
+            if (radioButton2.Checked)
+                textBox2.Text = ChangeBytes32(textBox1.Text);
+            if (radioButton3.Checked)
+                textBox2.Text = ChangeBytes64(textBox1.Text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+                textBox1.Text = ChangeBytes16(textBox2.Text);
+            if (radioButton2.Checked)
+                textBox1.Text = ChangeBytes32(textBox2.Text);
+            if (radioButton3.Checked)
+                textBox1.Text = ChangeBytes64(textBox2.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialogue = new OpenFileDialog();
+            dialogue.Title = "Open Little Endian File";
+            textBox1.Text = "Opening...";
+            if (dialogue.ShowDialog() != DialogResult.OK)
+            {
+                textBox1.Text = "";
+            }
+            if (dialogue.ShowDialog() == DialogResult.OK)
+            {
+                FileStream fs = new FileStream(dialogue.FileName, FileMode.Open);
+                int input;
+                textBox1.Text = "";
+                textBox2.Text = "";
+                for (int i = 0; (input = fs.ReadByte()) != -1; i++)
+                {
+                    String hex = string.Format("{0:X2}", input);
+                    textBox1.Text += hex;
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialogue = new OpenFileDialog();
+            dialogue.Title = "Open Big Endian File";
+            textBox2.Text = "Opening...";
+            if (dialogue.ShowDialog() != DialogResult.OK)
+            {
+                textBox2.Text = "";
+            }
+            if (dialogue.ShowDialog() == DialogResult.OK)
+            {
+                FileStream fs = new FileStream(dialogue.FileName, FileMode.Open);
+                int input;
+                textBox1.Text = "";
+                textBox2.Text = "";
+                for (int i = 0; (input = fs.ReadByte()) != -1; i++)
+                {
+
+                    String hex = string.Format("{0:X2}", input);
+                    textBox2.Text += hex;
+                }
+            }
+        }
+    }
+}
